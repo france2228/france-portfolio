1250436e8d21ebe3a14314f7812ce9bb10ef3e9d
france2228 <104053623+france2228@users.noreply.github.com>
2024-03-13 12:48:50 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Section from './components/SectionPage/Section';
+import Project from './components/ProjectPage/Project';
+import AboutPage from './components/AboutPage/AboutPage';
+import Experience from './components/ExperiencePage/Experience.js';
+import './components/css/style.css'
+import Skill from './components/SkillPage/Skill.js';
+import React, { useEffect} from "react";
 function App() {
+  let name ='France';
+  useEffect(() => {
+    document.title = "France Samaniego | Personal Website";  
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+   
+      <Header />
+      
+      <Section />
+      <Project />
+      <AboutPage />
+      <Experience />
+      <Skill />
+
+      <Footer />
+      
     </div>
   );
 }
